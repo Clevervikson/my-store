34950d04d62b337054aed8591f47d719d5d23226
Clevervikson <dclevict1@gmail.com>
2023-03-14 12:53:09 +0100

    Initial commit

---
+++ src/app/product-item/product-item.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-item',
+  templateUrl: './product-item.component.html',
+  styleUrls: ['./product-item.component.css']
+})
+export class ProductItemComponent {
+  title : string = 'Product Items';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

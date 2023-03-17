8f64450f8fbf2187f9203288d468fd059d4de660
Clevervikson <dclevict1@gmail.com>
2023-03-17 06:45:40 +0100

    update cart-item to display an item in a shopping cart

--- src/app/components/cart/cart-item/cart-item.component.ts
+++ src/app/components/cart/cart-item/cart-item.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { CartItem } from 'src/app/models/cartItem';
 
 @Component({
   selector: 'app-cart-item',
   templateUrl: './cart-item.component.html',
   styleUrls: ['./cart-item.component.css']
 })
-export class CartItemComponent {
+export class CartItemComponent implements OnInit {
+
+  @Input() item!: CartItem;
+  @Output() qchanged = new EventEmitter<void>();
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.item = new CartItem();
+  }
 
 }
